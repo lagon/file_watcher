b56c89c8e5c0b4d353fa41ab9dcd93e97d0b93c2
Miroslav Cepek <cepekmir@icloud.com>
2025-01-12 00:54:14 +0100

    Improvements

--- file_watcher.py
+++ file_watcher.py
@@ -1,8 +1,10 @@
 import argparse
 import hashlib
 import datetime
+import logging
 import queue as q
 import os
+import re
 import threading
 import time
 import typing as t
@@ -63,15 +65,27 @@ def file_fingerprint(input_queue: q.Queue, output_queue: q.Queue, idx: int) -> N
             output_queue.put(resp)
             break
 
+def _strip_base_path(path: str, base_path: str) -> str:
+    return path.replace(base_path, "")
 
-
-def list_and_process_directory(dir_path: str, input_queue: q.Queue, filedb: sqw.FileDBWrapper) -> None:
+def list_and_process_directory(dir_path: str, base_path: str, input_queue: q.Queue, filedb: sqw.FileDBWrapper, excludes: t.List[re.Pattern]) -> None:
     print(f"Working on {dir_path}")
     file_list = os.listdir(dir_path)
+
+    orig_file_list_length = len(file_list)
+    old_list = file_list.copy()
+    for exc in excludes:
+        file_list = list(filter(lambda f: exc.search(f) is None, file_list))
+
+    if orig_file_list_length != len(file_list):
+        print("Ted!")
+
     files_in_dir = list(filter(lambda fn: os.path.isfile(os.path.join(dir_path, fn)) , file_list))
     subdir_in_dir = list(filter(lambda fn: os.path.isdir(os.path.join(dir_path, fn)) , file_list))
 
     for fname in files_in_dir:
+        fpath_name_in_db = _strip_base_path(os.path.join(dir_path, fname), base_path)
+
         if filedb.does_file_exist(os.path.join(dir_path, fname)):
             print(f"{fname} already processed")
             continue
@@ -84,18 +98,19 @@ def list_and_process_directory(dir_path: str, input_queue: q.Queue, filedb: sqw.
         ))
 
     for dirname in subdir_in_dir:
-        list_and_process_directory(os.path.join(dir_path, dirname), input_queue, filedb)
+        list_and_process_directory(dir_path=os.path.join(dir_path, dirname), base_path=base_path, input_queue=input_queue, filedb=filedb, excludes=excludes)
 
     return
 
-def write_file_state(output_queue: q.Queue, filedb_path: str, num_workers: int) -> None:
-    filedb: sqw.FileDBWrapper = sqw.FileDBWrapper(filedb_path)
+def write_file_state(output_queue: q.Queue, filedb_path: str, base_dir: str, num_workers: int) -> None:
+    filedb: sqw.FileDBWrapper = sqw.FileDBWrapper(db_path=filedb_path, resume=True)
 
     while num_workers > 0:
         resp: FileFingerprintResponse = output_queue.get()
         if resp.success:
+            fname = resp.filename.replace(base_dir, "")
             filedb.record_file(
-                filename=resp.filename,
+                filename=fname,
                 sha384=resp.sha384,
                 file_size=resp.file_size
             )
@@ -104,20 +119,37 @@ def write_file_state(output_queue: q.Queue, filedb_path: str, num_workers: int) 
 
 
 def main():
-
     apars = argparse.ArgumentParser(description="Watch a directory for changes", add_help=True)
     apars.add_argument("--in_dir", dest="in_dir", type=str, help="Root directory to scan and explore", required=True)
     apars.add_argument("--filedb", dest="filedb", type=str, help="Where to store file hashes.", required=True)
+    apars.add_argument("--resume", dest="resume", type=str, choices=["True", "False"], help="It is OK to continue.", default="False", required=False)
+    apars.add_argument("--exclude", nargs="*", dest="exclude", type=str, action="append", help="It is OK to exclude.", default=[], required=False)
     apars.add_argument("--num_workers", dest="num_workers", type=int, help="How many processes to spawn", default=2)
     args = apars.parse_args()
 
+    _excludes = []
+    for excl in args.exclude:
+        _excludes.extend(excl)
+    args.exclude = _excludes
+
     print(f"Will work on directory {args.in_dir} and put the output in {args.filedb}")
+    args.resume = bool(args.resume)
 
     input_queue = q.Queue()
     output_queue = q.Queue()
-    filedb = sqw.FileDBWrapper(args.filedb)
-    filedb.write_start_metadata(dir_base=args.in_dir, start_time=datetime.datetime.now())
+    filedb = sqw.FileDBWrapper(args.filedb, resume=args.resume)
+    if filedb.is_resume():
+        stored_exclude_raw = filedb.get_metadata_value("excludes")
+        stored_exclude = stored_exclude_raw.split(";")
 
+        args.exclude = stored_exclude
+        logging.info(f"Using excludes from DB: {', '.join(stored_exclude)}")
+
+        filedb.update_start_metadata(latest_update_time=datetime.datetime.now())
+    else:
+        filedb.write_start_metadata(dir_base=args.in_dir, start_time=datetime.datetime.now(), excludes=args.exclude)
+
+    exclude_regex = [re.compile(exc) for exc in args.exclude]
     worker_threads = []
     for idx in range(args.num_workers):
         print(f"Starting thread {idx}")
@@ -125,11 +157,11 @@ def main():
         thr.daemon = False
         thr.start()
         worker_threads.append(thr)
-    writer_thread = threading.Thread(target=write_file_state, args=(output_queue, args.filedb, args.num_workers))
+    writer_thread = threading.Thread(target=write_file_state, args=(output_queue, args.filedb, args.in_dir, args.num_workers))
     writer_thread.daemon = False
     writer_thread.start()
 
-    list_and_process_directory(args.in_dir, input_queue, filedb)
+    list_and_process_directory(dir_path=args.in_dir, base_path=args.in_path, input_queue=input_queue, filedb=filedb, excludes=exclude_regex)
     print("All done, just waiting for workers to finish")
     for thr in worker_threads:
         input_queue.put(FileFingerprintRequest(requested_filename="", last_request=True))
--- sql_wrapper.py
+++ sql_wrapper.py
@@ -1,36 +1,82 @@
 import datetime
+import logging
 import os
 import sqlite3 as sql
 import typing as t
 
 class FileDBWrapper():
-    def __init__(self, db_path: str):
-        self._connection = sql.connect(db_path, autocommit=True)
-        sql_str = """CREATE TABLE IF NOT EXISTS file_state ( 
-filename TEXT NOT NULL PRIMARY KEY,
-directory TEXT NOT NULL,
-sha384 TEXT NOT NULL,
-filesize INTEGER NOT NULL
-)"""
-        self._connection.execute(sql_str)
-        self._connection.commit()
+    def __init__(self, db_path: str, resume: bool):
+        if os.path.exists(db_path) and not resume:
+            msg = "File database already exists. Use --resume to continue work."
+            logging.critical(msg)
+            raise FileExistsError(msg)
 
-        sql_str = """CREATE TABLE IF NOT EXISTS db_info ( 
-key TEXT PRIMARY KEY,
-value TEXT
-)"""
-        self._connection.execute(sql_str)
-        self._connection.commit()
+        if not os.path.exists(db_path) and resume:
+            logging.warning("The database for resuming does not exist. Starting with a clean one.")
+            resume = False
+
+        self._resume = resume
+
+        self._connection = sql.connect(db_path, autocommit=True)
+        if not self._resume:
+            sql_str = """CREATE TABLE IF NOT EXISTS file_state ( 
+    filename TEXT NOT NULL PRIMARY KEY,
+    directory TEXT NOT NULL,
+    sha384 TEXT NOT NULL,
+    filesize INTEGER NOT NULL
+    )"""
+            self._connection.execute(sql_str)
+            self._connection.commit()
+
+            sql_str = """CREATE TABLE IF NOT EXISTS db_info ( 
+    key TEXT PRIMARY KEY,
+    value TEXT
+    )"""
+            self._connection.execute(sql_str)
+            self._connection.commit()
+
+    def is_resume(self):
+        return self._resume
+
+    def _does_metadata_key_exist(self, key: str) -> bool:
+        sql_str = """SELECT key FROM db_info WHERE key = ?"""
+        result = self._connection.execute(sql_str, (key, ))
+        ret_vals = result.fetchall()
+        return len(ret_vals) == 1
+
+    def _write_or_update_single_metadatum(self, key: str, value: str) -> None:
+        if self._does_metadata_key_exist(key):
+            sql_str = """UPDATE db_info SET value = ? WHERE key = ?"""
+            self._connection.execute(sql_str, (key, value))
+            self._connection.commit()
+        else:
+            sql_str = """INSERT INTO db_info (key, value) VALUES (?, ?)"""
+            self._connection.execute(sql_str, (key, value))
+            self._connection.commit()
+        return
 
     def _write_single_metadatum(self, key: str, value: str) -> None:
         sql_str = """INSERT INTO db_info (key, value) VALUES (?, ?)"""
         self._connection.execute(sql_str, (key, value))
         self._connection.commit()
-        return
 
-    def write_start_metadata(self, dir_base: str, start_time: datetime.datetime) -> None:
+    def get_metadata_value(self, key: str) -> str:
+        sql_str = """SELECT value FROM db_info WHERE key = ?"""
+        result = self._connection.execute(sql_str, (key, ))
+        ret_vals = result.fetchall()
+        return ret_vals[0][0]
+
+    def update_start_metadata(self, latest_update_time: datetime.datetime) -> None:
+        num_restarts = int(self.get_metadata_value("number_restarts")) + 1
+        self._write_or_update_single_metadatum("number_restarts", str(num_restarts))
+        self._write_or_update_single_metadatum("last_resume_time", str(num_restarts))
+
+    def write_start_metadata(self, dir_base: str, start_time: datetime.datetime, excludes: t.List[str]) -> None:
         self._write_single_metadatum("dir_base", dir_base)
         self._write_single_metadatum("start_time", start_time.strftime("%Y-%m-%d %H:%M:%S"))
+        self._write_single_metadatum("last_resume_time", start_time.strftime("%Y-%m-%d %H:%M:%S"))
+        self._write_single_metadatum("number_restarts", "0")
+        self._write_single_metadatum("excludes", ";".join(excludes))
 
     def write_end_metadata(self, end_time: datetime.datetime) -> None:
         self._write_single_metadatum("end_time", end_time.strftime("%Y-%m-%d %H:%M:%S"))
